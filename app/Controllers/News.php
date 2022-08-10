739d793552672e85ed9de1dbdbd5336af72d19e4
lzbhaha123 <845381951@qq.com>
2022-08-10 18:24:03 +1200

    v1.1

--- app/Controllers/News.php
+++ app/Controllers/News.php
@@ -4,11 +4,11 @@ namespace App\Controllers;
 
 class News extends BaseController
 {
-    public function index()
+    public function index() // show news list
     {
         $model = model(NewsModel::class);
 
-        $data['list'] = $model->getNews();
+        $data['list'] = $model->getNews(); // get all news
         $data['title'] = "News List";
         
         return view('templates/header', $data)
@@ -20,7 +20,7 @@ class News extends BaseController
         $data['back'] = 1;
         $model = model(NewsModel::class);
         $data['title'] = "Create News";
-        if(!empty ($_POST['title'])){
+        if(!empty ($_POST['title'])){  // if $_POST has value, add this value into table
             
             $model->addNews($_POST);
             return redirect()->to('/news');
@@ -35,12 +35,12 @@ class News extends BaseController
     public function edit($id = null){
         $data['back'] = 1;
         $model = model(NewsModel::class);
-        if(!empty($_POST['id'])){
-            $model->updateNews($_POST);
+        if(!empty($_POST['id'])){  // if $_POST has value, update data
+            $model->updateNews($_POST); // update data base on its id
             return redirect()->to('/news');
         }else{
-            $data['article'] = $model->getNewsById($id);
-            if(!$data['article']){
+            $data['article'] = $model->getNewsById($id); // get data base on its id
+            if(!$data['article']){  // check if id is correct
                 throw new \CodeIgniter\Exceptions\PageNotFoundException("article edit");
             }
             $data['title'] = "Edit News";
@@ -66,8 +66,8 @@ class News extends BaseController
     {
         $data['back'] = 1;
         $model = model(NewsModel::class);
-        $data['article'] = $model->getNewsById($id);
-        if(!$data['article']){
+        $data['article'] = $model->getNewsById($id); // get data base on its id
+        if(!$data['article']){ // check if id is correct
             throw new \CodeIgniter\Exceptions\PageNotFoundException("article");
         }
         $data['title'] = "News Content";
